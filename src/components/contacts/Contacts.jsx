2d46cc5a48cbd06f590abf4ec50448fc1b3d3aee
Angel Ramirez <angel.andes.ramirez@gmail.com>
2023-03-21 16:31:10 -0400

    first commit - foundations for portfolio website

---
+++ src/components/contacts/Contacts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Contacts.css'
+
+const contacts = () => {
+  return (
+    <div>contacts</div>
+  )
+}
+
+export default contacts
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header.css'
+
+function header() {
+  return (
+    <div>header</div>
+  )
+}
+
+export default header
